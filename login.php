530859a6282e2bc84e5e567dfc3824d4f9dcae62
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-12-08 15:07:32 -0300

    Implementação das páginas de erro

--- login.php
+++ login.php
@@ -1,40 +1,66 @@
-<?php 
-$usuario = "root"; 
-$senha = "root";
-$database = "loginregistry"; 
-$host = "127.0.0.1:3306";
+<?php
+    $usuario = "root"; 
+    $senha = "root";
+    $database = "loginregistry"; 
+    $host = "127.0.0.1:3306";
 
-//Cria uma conexão
-$con = new mysqli($host, $usuario, $senha, $database);
+    $con = new mysqli($host, $usuario, $senha, $database);
 
-//Verifica a conexão
-if ($con->connect_error) {
+    if ($con->connect_error) {
     die("Falha na conexão: " . $con->connect_error);
-}
-
-// formulário com campos 'email' e 'senha'
-$email = isset($_GET['email']) ? $_GET['email'] : null; //login é o nome do meu objeto no input
-$senha = isset($_GET['senha']) ? $_GET['senha'] : null; //senha é o nome do meu objeto no input
-
-$sql = "SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'"; //mensagem para o banco de dados
-$result = $con->query($sql);
-
-if (!$result) {
-    die('Query failed: ' . mysqli_error($conn));
-}
-
-// Check if any rows were returned
-if (mysqli_num_rows($result) > 0) {
-    // Fetch and print the result
-    while ($row = mysqli_fetch_assoc($result)) {
-        print_r($row);
     }
-} else {
-    echo 'No results found.';
-}
 
-$con->close();
-?>
+    $email = isset($_GET['email']) ? $_GET['email'] : null;
+    $senha = isset($_GET['senha']) ? $_GET['senha'] : null; 
 
-</body>
-</html>
+    $sql = "SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'"; 
+    $result = $con->query($sql);
+
+    // var_dump($result);
+    // die;
+
+    if (mysqli_num_rows($result) > 0) {
+
+        while ($row = mysqli_fetch_assoc($result)) { 
+            echo '<h1>Olá, seja bem vindo(a) </h1>' . '<strong>' . $row["nome"] . '</strong>';
+            // $_SESSION['email'] = $email;
+            // $_SESSION['senha'] = $senha;
+        }
+
+    } else {
+    
+    echo "<h1>404 Not Found</h1>" . "<p>Usuário não encontrado. Por favor, verifique suas credenciais.</p>";
+    header('Location: manutenção.html');
+    exit();
+    // header("HTTP/1.0 404 Not Found");
+    // unset($_SESSION['email']) ;
+    // unset($_SESSION['senha']) ;
+    // 
+    }
+
+    $con->close();
+
+?>   
+
+<!-- tela de acesso do usuario:
+verifico no meu banco de dados se o usuario tem acesso ou não - ok
+(tela pra quando ele tiver acesso - bem vindo fulano de tal) - ok
+
+questão das session
+
+tela pra quando ele não tiver acesso - html com o erro (erro de credenciais erradas) - ok
+
+tambem criando paginas de erros http: - ok
+
+uma pra erro do servidor: 
+quando a pagina do servidor estiver em manutenção - qual pagina ? pagina de cadastro) -->
+
+<!-- //VERIFICAÇÃO DA SESSION
+//session_start();
+// if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
+//     unset($_SESSION['email']) ;
+//     unset($_SESSION['senha']) ;
+//     header('Location: front-end.html'); //HTTP/1.1 503 Service Unavailable
+// } 
+// $acesso = $_SESSION['email']; -->
+
