8cbc31bf9c4ddc07ecf5c810243c214d0e035ae6
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-12-15 10:17:14 -0300

    .

--- login.php
+++ login.php
@@ -1,66 +1,64 @@
 <?php
-    $usuario = "root"; 
-    $senha = "root";
-    $database = "loginregistry"; 
-    $host = "127.0.0.1:3306";
+    // Seção de Configuração
+    
 
-    $con = new mysqli($host, $usuario, $senha, $database);
-
-    if ($con->connect_error) {
-    die("Falha na conexão: " . $con->connect_error);
+    // Se o usuario clicar pra sair, é direcionado para pagina principal
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        if ($_POST["val1"] == "1"){
+            login();
+        }
+        if ($_POST["val1"] == "2"){
+            logout();
+        }
+        
     }
 
-    $email = isset($_GET['email']) ? $_GET['email'] : null;
-    $senha = isset($_GET['senha']) ? $_GET['senha'] : null; 
+    function login(){
+        $usuario = "root";
+        $senha = "root";
+        $database = "loginregistry";
+        $host = "127.0.0.1:3306";
 
-    $sql = "SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'"; 
-    $result = $con->query($sql);
+        $con = new mysqli($host, $usuario, $senha, $database);
 
-    // var_dump($result);
-    // die;
-
-    if (mysqli_num_rows($result) > 0) {
-
-        while ($row = mysqli_fetch_assoc($result)) { 
-            echo '<h1>Olá, seja bem vindo(a) </h1>' . '<strong>' . $row["nome"] . '</strong>';
-            // $_SESSION['email'] = $email;
-            // $_SESSION['senha'] = $senha;
+        if ($con->connect_error) {
+        die("Falha na conexão: " . $con->connect_error);
         }
 
-    } else {
+        //Processamento de Login
+        $email = isset($_POST['email']) ? $_POST['email'] : null;
+        $senha = isset($_POST['senha']) ? $_POST['senha'] : null;
+
+        $sql = "SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'";
+        $result = $con->query($sql);
+
+        $con->close();
+        // var_dump($result);
+        // die;
+
+        session_start();
+
+        if (mysqli_num_rows($result) > 0) {
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $_SESSION['email'] = $email;
+                $_SESSION['senha'] = $senha;
+                header('Location: dashboard.html');
+                exit;
+            } 
+
+        }else { 
+            unset($_SESSION['email']);  
+            unset($_SESSION['senha']);
+            header('Location: 404.html');    
+            exit;
+        }
+        }
     
-    echo "<h1>404 Not Found</h1>" . "<p>Usuário não encontrado. Por favor, verifique suas credenciais.</p>";
-    header('Location: manutenção.html');
-    exit();
-    // header("HTTP/1.0 404 Not Found");
-    // unset($_SESSION['email']) ;
-    // unset($_SESSION['senha']) ;
-    // 
-    }
-
-    $con->close();
-
-?>   
-
-<!-- tela de acesso do usuario:
-verifico no meu banco de dados se o usuario tem acesso ou não - ok
-(tela pra quando ele tiver acesso - bem vindo fulano de tal) - ok
-
-questão das session
-
-tela pra quando ele não tiver acesso - html com o erro (erro de credenciais erradas) - ok
-
-tambem criando paginas de erros http: - ok
-
-uma pra erro do servidor: 
-quando a pagina do servidor estiver em manutenção - qual pagina ? pagina de cadastro) -->
-
-<!-- //VERIFICAÇÃO DA SESSION
-//session_start();
-// if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
-//     unset($_SESSION['email']) ;
-//     unset($_SESSION['senha']) ;
-//     header('Location: front-end.html'); //HTTP/1.1 503 Service Unavailable
-// } 
-// $acesso = $_SESSION['email']; -->
+    function logout(){
+            session_destroy();
+            header('Location: index.html');
+            exit;
+        }
+?>
 
