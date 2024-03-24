865f64147fd2ef93bef8dddca65f6cab6dfc75d7
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2024-03-24 16:03:43 -0300

    verificação de login no banco de dados
    - Implementa verificação de usuário cadastrado no banco de dados
    - Permite acesso apenas para usuários cadastrados

--- login.php
+++ login.php
@@ -1,64 +1,75 @@
 <?php
-    // Seção de Configuração
-    
+    // conexão com o banco de dados 
+    $dbHost = "127.0.0.1:3306";
+    $dbUsername = "root";
+    $dbPassword = "root";
+    $dbName = "loginregistry";       
 
-    // Se o usuario clicar pra sair, é direcionado para pagina principal
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        if ($_POST["val1"] == "1"){
-            login();
-        }
-        if ($_POST["val1"] == "2"){
-            logout();
-        }
-        
+    $con = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    if ($con->connect_error) {
+        die("Falha na conexão: " . $con->connect_error);
     }
 
-    function login(){
-        $usuario = "root";
-        $senha = "root";
-        $database = "loginregistry";
-        $host = "127.0.0.1:3306";
+    //Processamento de Login
+    $email = isset($_POST['email']) ? $_POST['email'] : null;
+    $senha = isset($_POST['senha']) ? $_POST['senha'] : null;
 
-        $con = new mysqli($host, $usuario, $senha, $database);
+    $sql = "SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'";
+    $result = $con->query($sql);
 
-        if ($con->connect_error) {
-        die("Falha na conexão: " . $con->connect_error);
-        }
-
-        //Processamento de Login
-        $email = isset($_POST['email']) ? $_POST['email'] : null;
-        $senha = isset($_POST['senha']) ? $_POST['senha'] : null;
-
-        $sql = "SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'";
-        $result = $con->query($sql);
-
-        $con->close();
-        // var_dump($result);
-        // die;
-
-        session_start();
-
-        if (mysqli_num_rows($result) > 0) {
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                $_SESSION['email'] = $email;
-                $_SESSION['senha'] = $senha;
-                header('Location: dashboard.html');
-                exit;
-            } 
-
-        }else { 
-            unset($_SESSION['email']);  
-            unset($_SESSION['senha']);
-            header('Location: 404.html');    
-            exit;
-        }
-        }
+    if (mysqli_num_rows($result) > 0) {
+        //se encontrar um usuario com as credenciais fornecidas acima
+        $usuario = $result->fetch_assoc();
+        $nomeUsuario = $usuario['nome'];
+        echo 'bem vindo ' . $nomeUsuario;
+    } else {
+        echo "Credenciais inválidas. Por favor, tente novamente.";
+    }
     
-    function logout(){
-            session_destroy();
-            header('Location: index.html');
-            exit;
-        }
-?>
+
+    $con->close();
+    
+
+    // // Se o usuario clicar pra sair, é direcionado para pagina principal
+    // if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    //     if ($_POST["val1"] == "1"){
+    //         login();
+    //     }
+    //     if ($_POST["val1"] == "2"){
+    //         logout();
+    //     }
+        
+    // }
+
+//     function login(){
+        
+//         // var_dump($result);
+//         // die;
+
+//         session_start();
+
+//         if (mysqli_num_rows($result) > 0) {
+
+//             while ($row = mysqli_fetch_assoc($result)) {
+//                 $_SESSION['email'] = $email;
+//                 $_SESSION['senha'] = $senha;
+//                 header('Location: dashboard.html');
+//                 exit;
+//             } 
+
+//         }else { 
+//             unset($_SESSION['email']);  
+//             unset($_SESSION['senha']);
+//             header('Location: 404.html');    
+//             exit;
+//         }
+//     }
+    
+//     function logout(){
+//         session_destroy();
+//         header('Location: index.html');
+//         exit;
+//     }
+// ?>
 
